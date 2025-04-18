112f36f9612134d59c602c06dda161d90e68f769
manavdharmd <manavdharmd@gmail.com>
2025-04-18 20:06:27 +0530

    Assignment1

---
+++ Assignment1.py
@@ -0,0 +1,13 @@
+# Task 1
+Input_1 = float(input("Type Input 1: "))
+Input_2 = float(input("Type Input 2: "))
+
+print("Addition:",Input_1+Input_2)
+print("Subtraction:",Input_1-Input_2)
+print("Multiplication:",Input_1*Input_2)
+print("Divsion:",Input_1/Input_2)
+
+# Task 2
+F_Name = input("Enter your first name : ")
+L_Name = input("Enter your last name : ")
+print(f"Hello,{F_Name} {L_Name}! Welcome to Python program")
